94e5f5d00223a21c532548205762282c722aeeb5
asmadek <alma2610@ya.ru>
2019-07-29 22:04:47 +0500

    remove devtools

--- src/background.js
+++ src/background.js
@@ -37,7 +37,6 @@ function createWindow () {
       nodeIntegration: true
     }
   })
-  win.webContents.openDevTools()
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
